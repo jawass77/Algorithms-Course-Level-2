ff4ab17ed3194e9abba4c49d1976ddcf5d509985
Ali Adel Abdullah jawass <jawass774672181@gmail.com>
2025-02-27 11:34:11 +0300

    Check number in array

---
+++ #35.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+int ReadPostiveNumber(string text)
+{
+
+    int num;
+    do
+    {
+        cout << text << endl;
+        cin >> num;
+    } while (num < 0);
+
+    return num;
+}
+
+int randomNumber(int From, int To)
+{
+    int randNum = rand() % (To - From + 1) + From;
+    return randNum;
+}
+
+void readArryWithRandomNumbers(int arry[], int lenth)
+{
+    for (int i = 0; i < lenth; i++)
+    {
+        arry[i] = randomNumber(10, 100);
+    }
+}
+
+void printArry(int arry[], int lenth)
+{
+    cout << "array 1 enemints: \n";
+    for (int i = 0; i < lenth; i++)
+    {
+        cout << arry[i] << " ";
+    }
+    cout << endl
+         << endl;
+
+}
+
+bool CheckNumberInArray(int Array[], int length, int search)
+{
+    for (int i = 0; i < length; i++)
+    {
+        if (Array[i] == search)
+        {
+            return true;
+        }
+    }
+    return false;
+
+}
+
+int main()
+{
+    int Array[100];
+    int length=ReadPostiveNumber("enter number from 1 to 100");
+
+    readArryWithRandomNumbers(Array,length);
+
+    printArry(Array,length);
+
+    int search=ReadPostiveNumber("please enter a number to search for ? ");
+
+    
+    cout<<"Number you are looking for is : "<<search<<endl;
+
+    bool result =CheckNumberInArray(Array,length,search);
+
+    if (result)
+    {
+        cout <<"Yes, The number is found :-)"<<endl;
+    }else
+    {
+        cout <<"NO, The number is not found :-("<<endl;
+    }
+
+
+}
+
